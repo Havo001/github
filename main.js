461405f7ff04e0d5855f3d716e5e8599249bdbd9
Havo001 <havo535ovah@gmail.com>
2025-03-22 17:37:06 +0300

    сумка

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 let menu = document.querySelector(".fixed-menu");
 
 menu.addEventListener('click', function(event){
-   let clickTarget = event.target;
-   let activeBtn = document.querySelector('.active');
-   
-   if (clickTarget.classList.contains('nav-link')){
-       clickTarget.classList.add('active');
-       activeBtn.classList.remove('active');
-   }
-});
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active');
+    if (clickTarget.classList.contains('nav-link') ){
+        clickTarget.classList.add('active');
+        activeBtn.classList.remove('active');
+ 
+        let mobile = document.querySelector('.mobile-menu');
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide');
+        }
+ 
+    }
+    let body = document.querySelector("body")
+    let mobileMenu = document.querySelector(".mobile-menu");
+    if (!body.classList.contains("off-scroll")&&
+                !mobileMenu.classList.contains('hide')){
+        body.classList.add("off-scroll");
+    }
+    else{
+        body.classList.remove("off-scroll")
+    }
+ });
 let classLink ='.main-link'
 window.onscroll = function() {
     let h = document.documentElement.clientHeight;
@@ -34,3 +48,8 @@ if (!newActiveBtn.classList.contains('active')){
     newActiveBtn.classList.add('active');
     activeBtn.classList.remove('active')
 };
+document.querySelector(".mobile-button").addEventListener("click",
+function(event)
+{
+    document.querySelector(".mobile-menu").classList.toggle("hide");
+});
